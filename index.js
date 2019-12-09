b12a1ad33faba27474682dbf3911e69346d74fa0
Alex <Magana@Alexs-MacBook-Air.local>
2019-12-09 13:50:17 -0600

    Almost answered last problem

--- index.js
+++ index.js
@@ -286,8 +286,15 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(max) {
+  let count = 0;
+  return function counter() {
+    if (count <= max) {
+      return count++;
+    } else {
+      return count = 0;
+    }
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
